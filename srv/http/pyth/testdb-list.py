f347422226d0c864898a6769579ed23213f648c8
matthew luke <matt.miccio@gmail.com>
2013-12-10 12:29:52 -0500

    CSS

--- srv/http/pyth/testdb-list.py
+++ srv/http/pyth/testdb-list.py
@@ -32,7 +32,7 @@ with db:
 while 1:
 
 	if (GPIO.input(12) and x == 1):
-		print('Input went HIGH')
+#		print('Input went HIGH')
 
 		localtime = time.asctime( time.localtime(time.time()) )
 		senslist = [localtime, sens]
@@ -42,17 +42,17 @@ while 1:
 		db_count = db_count + 1
 
 	elif (not GPIO.input(12) and  x == 0):
-		print('Input went LOW')
+#		print('Input went LOW')
 		x = 1
 
 	if (db_count == 1):
-		print(biglist)
+#		print(biglist)
 
 		cur.executemany('INSERT INTO sensordat2 VALUES (%s, %s)', biglist)
 
 		cur.execute("SELECT * FROM sensordat2")
-		for reading in cur.fetchall():
-			print str(reading[0])+"	"+str(reading[1])
+#		for reading in cur.fetchall():
+#			print str(reading[0])+"	"+str(reading[1])
 		db.commit()
 
 #		print("Database reached 5 entries, DB will be cleared")
